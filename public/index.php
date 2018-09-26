40f788a9d4cf940493e2a8b71e4f2c36cf3c9964
ko.popov <ko.popov@teamber.ru>
2018-09-26 10:49:25 +0300

    initial commit

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+echo 'Hello somebody!';
+
+?>
+
+<h3>TODO list:</h3>
+
+<ol>
+    <li>Сформировать образы php и nginx и настроить их совместную работу посредством docker-compose</li>
+</ol>
