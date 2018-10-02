e4bd7fb42a89da569fdf1ce199e216aae9f05426
ko.popov <ko.popov@teamber.ru>
2018-10-02 10:09:01 +0300

    add image generation library

--- public/index.php
+++ public/index.php
@@ -1,11 +1,30 @@
 <?php
 
-echo 'Hello somebody!';
+require __DIR__ . '/../vendor/autoload.php';
 
+use LasseRafn\InitialAvatarGenerator\InitialAvatar;
+
+$avatar = new InitialAvatar();
+
+function random_color_part() {
+    return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
+}
+
+function random_color() {
+    return random_color_part() . random_color_part() . random_color_part();
+}
+
+$image = $avatar
+    ->name('Lasse Rafn')
+    ->fontSize(0.5)
+    ->size(96)
+    ->background('#' . random_color())
+    ->color('#' . random_color())
+    ->generate();
+
+$imageName = __DIR__ . '/images/' . 'demo' . '.png';
+
+$image->save($imageName);
 ?>
 
-<h3>TODO list:</h3>
-
-<ol>
-    <li>Сформировать образы php и nginx и настроить их совместную работу посредством docker-compose</li>
-</ol>
+<img src="images/demo.png"/>
